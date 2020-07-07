70b8679f042937020ba714a540be1545f4f56a0f
David Omar Flores Chávez <david@davidomar.com>
2020-07-06 20:14:02 -0500

    Handle the conditional flags `migrate` and `drop`
    The `drop` flag runs everytime a new migration is going to be made. So
    there **must** be a model to migrate to try to drop it. Models that get
    deleted won't get dropped in the database, since there's no reference to
    it.

--- database/database.go
+++ database/database.go
@@ -15,7 +15,9 @@ import (
 
 var (
 	// DBConn handles the connection to share between packages
-	DBConn *gorm.DB
+	DBConn    *gorm.DB
+	dbMigrate bool
+	dbDrop    bool
 )
 
 // GetConnection creates a connection with the database using the
@@ -24,25 +26,52 @@ func GetConnection() {
 	var datastore config.Datastore = config.GetDatabaseConfiguration()
 
 	var err error
-	DBConn, err = gorm.Open("postgres", "sslmode=disable host="+datastore.Address+" port="+datastore.Port+" user="+datastore.User+" dbname="+datastore.Database+" password="+datastore.Password)
+	DBConn, err = gorm.Open(
+		"postgres",
+		"sslmode=disable"+
+			" host="+datastore.Address+
+			" port="+datastore.Port+
+			" user="+datastore.User+
+			" dbname="+datastore.Database+
+			" password="+datastore.Password)
 
 	if err != nil {
 		log.Fatalf("Error: Connection to database %v", err)
 	}
+
+	dbMigrate = datastore.Migrate
+	dbDrop = datastore.Drop
 
 	log.Print("Successfully: Connection to database")
 }
 
 // Migrate creates the necessary tables to fit the app types
 func Migrate() {
+	if !dbMigrate {
+		log.Print("Won't migrate models")
+		return
+	}
+	log.Print("Migrating models")
 	migrate(&models.Problem{})
 	migrate(&models.Lesson{})
 }
 
 // Delete existing table and automigrate from models
 func migrate(table interface{}) {
-	DBConn.DropTableIfExists(table)
-	DBConn.AutoMigrate(table)
+	log.Printf(" - %T", table)
+
+	if dbDrop {
+		if err := DBConn.DropTableIfExists(table).Error; err != nil {
+			log.Print("    - Failed drop")
+		} else {
+			log.Print("    - Successful drop")
+		}
+	}
+	if err := DBConn.AutoMigrate(table).Error; err != nil {
+		log.Printf("    - Failed migration")
+	} else {
+		log.Printf("    - Successful migration")
+	}
 }
 
 // CloseConnection closes the connection to the database
